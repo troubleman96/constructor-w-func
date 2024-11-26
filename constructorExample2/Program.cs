68aca355b4977f148b95ed774548dfb616a5b24c
troubleman96 <itslugenge96@gmail.com>
2024-11-26 10:04:11 +0300

    first commit

---
+++ constructorExample2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Exampe
+{
+    class Employee
+    {
+        public int id;
+        public string name;
+        public float salary;
+
+        public Employee(int i, string n, float s)
+        {
+            id = i;
+            name = n;
+            salary = s;
+        }
+
+        public void display()
+        {
+            Console.WriteLine(id + " " + name + " " + salary );
+        }
+
+        static void Main(string[] arg)
+        {
+            Employee e1 = new Employee(1, "lugenge", 1200000f);
+            
+            e1.display();
+        }
+    }
+}
